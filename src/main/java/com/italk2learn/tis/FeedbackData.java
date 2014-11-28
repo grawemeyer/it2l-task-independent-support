e343955c071974ca6b59654a46e97e6de56594c8
Beate Grawemeyer <b.grawemeyer@gmail.com>
2014-11-28 15:45:26 +0000

    - include full stop at reflective message

--- src/main/java/com/italk2learn/tis/FeedbackData.java
+++ src/main/java/com/italk2learn/tis/FeedbackData.java
@@ -5,7 +5,7 @@ public class FeedbackData {
 	public static final String affectBoost1 = "It may be hard, but keep going.";
 	public static final String mathsReminder1 = "Can you explain that again using the terms denominator, numerator?";
 	public static final String reflective1 = "What are you trying to do in this task?";
-	public static final String reflective2 = "Please think of how you could answer the task another way";
+	public static final String reflective2 = "Please think of how you could answer the task another way.";
 	
 
 }
