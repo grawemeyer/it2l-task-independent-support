641da2ce5d580afe89dc7c46f1405bdfaf328fec
Beate Grawemeyer <b.grawemeyer@gmail.com>
2015-02-24 12:35:39 +0000

    remove old messages

--- src/main/java/com/italk2learn/tis/FeedbackData.java
+++ src/main/java/com/italk2learn/tis/FeedbackData.java
@@ -33,14 +33,4 @@ public class FeedbackData {
 	
 	public static String mathsReminder = "Please explain that again using the words denominator and numerator.";
 	
-	
-	/*public static final String affectBoost1 = "It may be hard, but keep going.";
-	public static final String mathsReminder1 = "Can you explain that again using the terms denominator, numerator?";
-	public static final String reflective1 = "What are you trying to do in this task?";
-	public static final String reflective2 = "Please think of how you could answer the task another way.";
-	*/
-	
-	
-	
-
 }
